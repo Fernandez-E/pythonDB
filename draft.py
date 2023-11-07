04283ab3ea617fbf3490b91bc28d693575f12567
Eric G. Fernandez A. S. <efernandezalbuq@gmail.com>
2023-11-07 14:52:23 +0000

    manyrows

--- draft.py
+++ draft.py
@@ -1,7 +1,42 @@
 import sqlite3
 from functions.create_db import create_db
 from functions.create_table import create_table
-from functions.insert_row import insert_one_row
+from functions.insert_row import insert_one_row, insert_many_rows
+
+
+DB = 'database3.db'
+TB = 'tabela'
+
+conn = sqlite3.connect('database3.db')
+cursor = conn.cursor()
+
+cursor.executemany(
+    """
+    INSERT INTO tabela (nome, idade) VALUES (?, ?)
+    """,
+    [('AAA', 33),
+    ('BBB', 55),
+    ('ccc', 11)]
+)
+
+conn.commit()
+
+insert_many_rows(DB, TB, 'nome, idade', [('srydfg', 22), ('dfjgur', 55)])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 DB = 'database3.db'
 TB = 'tabela'
--- functions/insert_row.py
+++ functions/insert_row.py
@@ -35,3 +35,40 @@ def insert_one_row(
 
     conn.commit()
     conn.close()
+
+
+def insert_many_rows(
+        database_name:str,
+        table_name:str,
+        columns_name:str,
+        values_list: list
+) -> None:
+    
+    """ Insert rows
+    Args:
+        database_name (str): A database's name    
+        table_name (str): A table's name  
+        columns (str): Columns query 
+        columns_name (str): Columns to insert values
+        values_list (str):
+
+        Example:
+        ```{python}
+        insert_one_row (
+            'test.db', 
+            'tabela', 
+            'id, name, age',
+            [(1, "Eric", 28)], [(2, "qqqq", 28)]
+        )
+        ```
+    """
+        
+    conn = sqlite3.connect(database_name)
+    cursor = conn.cursor()
+
+    query = f"INSERT INTO {table_name} ({columns_name}) VALUES (?, ?)"
+
+    cursor.executemany(query, values_list)
+
+    conn.commit()
+    conn.close()
